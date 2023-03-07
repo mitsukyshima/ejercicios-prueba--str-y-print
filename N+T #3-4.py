f3c9259af296f88341eb5f351875f02035a0eb0a
mitsuky <110051985+mitsukyshima@users.noreply.github.com>
2023-03-07 18:01:47 +0200

    Add files via upload

---
+++ N+T #3-4.py
@@ -0,0 +1,30 @@
+print ("prueba de susana")
+mensaje = "susana tiene"
+Mensaje = 5
+Mensaje -= 2
+print (mensaje)
+print (Mensaje )
+
+
+numero = 3
+numeroo = 1
+calcul = numero + numeroo
+calcul = str (calcul)
+print ("Y su amigo tiene " + calcul)
+
+print ("tkl")
+edad = 10
+edadd = 7
+edades = edad + edadd
+edades = str (edades)
+pansito = "  "
+print ("pablo tiene" + pansito + edades)
+
+
+print ("segunda prueba ")
+zapato = 5
+zaapato = 5
+regla = zapato + zaapato
+susu = ' '
+regla = str (regla)
+print ("jajajaja si lo consegui >:) > " + susu + regla )
